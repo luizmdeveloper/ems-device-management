dfd63184eeaed3221107429f943b9d8672a7e99e
Luiz Mário <azure@inforio.com.br>
2025-04-09 22:01:41 -0300

    Initial base code

---
+++ src/main/java/br/com/luizmariodev/ems/device/management/EmsDeviceManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizmariodev.ems.device.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsDeviceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsDeviceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizmariodev/ems/device/management/EmsDeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizmariodev.ems.device.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsDeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
